45dc5f29f84dfd73d01e28e3b893eecb77e55c11
phuong <alphn2015@gmail.com>
2023-03-11 23:27:03 +0000

    MAINT: project setup

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPassthroughCopy("./src/styles/style.css");
+    eleventyConfig.addPassthroughCopy("src/contents/img");
+    eleventyConfig.addWatchTarget("./src/styles/style.css");
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        },
+    };
+};
