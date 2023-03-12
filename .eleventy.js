d4e0cd12f67fd39d9c4d7af7f30cdba0b77ce0fb
phuong <alphn2015@gmail.com>
2023-03-12 13:20:36 +0000

    ENH: added style for project pages

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPassthroughCopy("./src/styles/style.css");
+    eleventyConfig.addPassthroughCopy("./src/styles/projects_style.css");
     eleventyConfig.addPassthroughCopy("src/contents/img");
     eleventyConfig.addWatchTarget("./src/styles/style.css");
     return {
